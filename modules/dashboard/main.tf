ed5f3279f1975b6d0ad88625c88a40eb34d5b8cf
vsmac <vsfusesf@gmail.com>
2021-01-09 06:18:39 +0530

    adding disk gcp terraform

--- modules/dashboard/main.tf
+++ modules/dashboard/main.tf
@@ -4,15 +4,7 @@ provider "google" {
   version = ">= 3.23.0"
 }
 
-variable "project_id" {
-  description = "The ID of the project in which the dashboard will be created."
-  type        = string
-}
 
-variable "dashboard_json_file" {
-  description = "The JSON file of the dashboard."
-  type        = string
-}
 
 resource "google_project_service" "enable_destination_api" {
   project            = var.project_id
@@ -25,19 +17,3 @@ resource "google_monitoring_dashboard" "dashboard" {
   project        = var.project_id
 }
 
-output "project_id" {
-  value = var.project_id
-}
-
-output "resource_id" {
-  description = "The resource id for the dashboard"
-  value       = google_monitoring_dashboard.dashboard.id
-}
-
-output "console_link" {
-  description = "The destination console URL for the dashboard."
-  value       = join("", ["https://console.cloud.google.com/monitoring/dashboards/custom/",
-                          element(split("/", google_monitoring_dashboard.dashboard.id), 3),
-                          "?project=",
-                          var.project_id])
-}
