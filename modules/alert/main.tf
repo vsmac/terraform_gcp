d1149c43244c1b21426a8ccf8eed9f6bd2920627
Vishnu Gaur <44326343+vsmac@users.noreply.github.com>
2021-01-11 22:08:03 +0530

    Update main.tf

--- modules/alert/main.tf
+++ modules/alert/main.tf
@@ -1,20 +1,24 @@
-resource "google_monitoring_alert_policy" "alert_policy" {
-  display_name = "My Alert Policy"
-  combiner     = "OR"
+
+  display_name = "Test Notification Channel"
+  type         = "email"
+  labels = {
+    email_address = "xx@gmail.com"
+  }
+}
+resource "google_monitoring_alert_policy" "backend_restart_count_alert" {
+  combiner              = "OR"
+  display_name          = "Backend pod keeps restarting"
+  notification_channels = [google_monitoring_notification_channel.basic.id]
   conditions {
-    display_name = "test condition"
+    display_name = "Restart count"
     condition_threshold {
-      filter     = "metric.type=\"compute.googleapis.com/instance/disk/write_bytes_count\" AND resource.type=\"gce_instance\""
-      duration   = "60s"
-      comparison = "COMPARISON_GT"
+      filter          = "resource.type=\"gce_instance\" AND metric.type=\"agent.googleapis.com/memory/percent_used\" "
+      duration        = "60s"
+      comparison      = "COMPARISON_GT"
+      threshold_value = 50
       aggregations {
         alignment_period   = "60s"
-        per_series_aligner = "ALIGN_RATE"
       }
     }
-  }
-
-  user_labels = {
-    foo = "bar"
   }
 }
